add64dc51045c1f0ca29087bbf182fe28422f860
Kyle VanTassell <octicon@gmail.com>
2022-06-20 20:44:35 -0500

    q5-6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -25,6 +25,8 @@ console.log(supplyChanges);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push(25);
+console.log(supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -32,6 +34,25 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
+console.log(supplyChanges);
+for (i = 0; i < supplyChanges.length; i++) {
+    console.log(supplyChanges[i]);
+    if (supplyChanges[i] > 0) {
+        console.log(`'Added ${supplyChanges[i]} parts.'`);
+    }
+    if (supplyChanges[i] == 0) {
+        console.log('No Change.');
+    }
+    if (supplyChanges[i] < 0) {
+        console.log(`'Removed ${supplyChanges[i]} parts.'`);
+    }
+
+}
+
+
+
+
+
 
 
 // STRETCH GOALS
